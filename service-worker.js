0f8467a43e995646da8ef7eb3cd89e82a8e57b6f
gabrielbailly <158313900+gabrielbailly@users.noreply.github.com>
2024-12-14 20:08:21 +0100

    update

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,42 @@
+const CACHE_NAME = 'asistente-virtual-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icon-192x192.png',
+    '/icon-512x512.png'
+];
+
 self.addEventListener('install', event => {
-  console.log('Service Worker instalado');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
